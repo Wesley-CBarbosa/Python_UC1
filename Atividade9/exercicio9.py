8fcf51f1048e3fa93f033dbe98ccd172061ae867
WesleyB-926 <wesleycravo926@gmail.com>
2025-07-19 16:40:52 -0300

    Praticando pull request - exercicio9

---
+++ Atividade9/exercicio9.py
@@ -0,0 +1,8 @@
+# Exercício 9: Verifique se um número está entre 10 e 50.
+
+num = int(input("Informe um valor: "))
+
+if num >= 10 and num <= 50:
+  print("O número está correto.")
+else:
+  print("O número está incorreto.")
