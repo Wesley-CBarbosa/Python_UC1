a798d3ee622ff50adf753ecdb3897042a0973537
WesleyB-926 <wesleycravo926@gmail.com>
2025-07-20 15:59:13 -0300

    Praticando pull request - exercicio20

---
+++ Atividade20/exercicio20.py
@@ -0,0 +1,7 @@
+# Exercício 20: Retorne 'Aprovado' se a nota for maior ou igual a 7, senão 'Reprovado'.
+
+nota = float(input("Informe um valor: "))
+
+status = "Aprovado" if nota >= 7 else "Reprovado"
+
+print(status)
