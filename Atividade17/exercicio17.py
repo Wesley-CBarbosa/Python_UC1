96e3e97903428092de8f870b6889b30d4ab367ba
WesleyB-926 <wesleycravo926@gmail.com>
2025-07-20 15:48:25 -0300

    Praticando pull request - exercicio17

---
+++ Atividade17/exercicio17.py
@@ -0,0 +1,7 @@
+# Exercício 17: Verifique se um número é positivo ou negativo usando if ternário.
+
+num = int(input("Informe um valor: "))
+
+status = "Número positivo." if num >= 0 else "Número negativo."
+
+print(status)
