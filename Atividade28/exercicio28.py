c1bd186b2e9c078038cdda141d4cc2e61388fcc1
WesleyB-926 <wesleycravo926@gmail.com>
2025-07-21 12:39:17 -0300

    Praticando pull request - exercicio28

---
+++ Atividade28/exercicio28.py
@@ -0,0 +1,13 @@
+# Exercício 28: Crie um programa que solicite 5 números ao usuário e exiba a soma deles.
+
+print("Digite 5 números e veja a soma deles.")
+
+a = int(input("\nDigite o primeiro número: \n"))
+b = int(input("\nDigite o segundo número: \n"))
+c = int(input("\nDigite o terceiro número: \n"))
+d = int(input("\nDigite o quarto número: \n"))
+e = int(input("\nDigite o quinto número: \n"))
+
+soma = a + b + c + d + e
+
+print(f"A soma total dos valores é:", soma)
