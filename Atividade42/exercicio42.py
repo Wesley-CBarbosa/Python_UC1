53ab757944b02e229f35d0a337481d29ad934036
WesleyB-926 <wesleycravo926@gmail.com>
2025-07-23 14:11:24 -0300

    Praticando pull request - exercicio42

---
+++ Atividade42/exercicio42.py
@@ -0,0 +1,8 @@
+# Exercício 42: Verifique se um número existe dentro de uma lista.
+
+num = [1, 2, 3, 4, 5]
+
+if 3 in num:
+  print("O número está na lista.")
+else:
+  print("O número não está na lista.")
