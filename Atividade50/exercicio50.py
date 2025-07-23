90fc2c79a4de1c0ba797f1469130dac4b2d51569
WesleyB-926 <wesleycravo926@gmail.com>
2025-07-23 14:33:04 -0300

    Praticando pull request - exercicio50

---
+++ Atividade50/exercicio50.py
@@ -0,0 +1,6 @@
+# Exercício 50: Verifique se uma chave existe no dicionário.
+
+pessoas = {'João': 30, 'Maria': 25, 'Pedro': 35, 'Ana': 28, 'Carlos': 40}
+
+if 'João' in pessoas:
+  print("A chave existe no dicionário.")
